61fd44fe9ad1d89ac76c750627f3851e177e76fc
Matt Gildner <mattgildner101@gmail.com>
2025-06-10 23:12:11 -0700

    chore: add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+// src/components/footer.tsx
+'use client';
+
+export default function Footer() {
+  return (
+    <footer className="text-center text-sm text-gray-500 py-4">
+      Deployed from: {process.env.NEXT_PUBLIC_DEPLOY_FROM}
+    </footer>
+  );
+}
