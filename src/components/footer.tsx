254676e65b08e82e46272f68f64fd8b3e0d234bc
Matt Gildner <mattgildner101@gmail.com>
2025-06-10 23:26:53 -0700

    fixed vercel 2

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@
 export default function Footer() {
   return (
     <footer className="text-center text-sm text-gray-500 py-4">
-      Deployed from: {process.env.NEXT_PUBLIC_DEPLOY_FROM}
+      Deployed from1: {process.env.NEXT_PUBLIC_DEPLOY_FROM}
     </footer>
   );
 }
