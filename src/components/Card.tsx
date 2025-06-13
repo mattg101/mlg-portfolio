425ae062b3cd818184c8e011f02a93c8edb735f6
mattg101 <mattgildner101@gmail.com>
2025-06-13 08:14:38 -0700

    feat: Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,27 +1,20 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
+'use client';
 
-import Link from 'next/link';
+
 import { motion } from 'framer-motion';
-import { ReactNode } from 'react';
+import Link from 'next/link';
 
-interface CardProps {
-  title: string;
-  description: string;
-  href?: string;
-  children?: ReactNode;
-}
-
-export default function Card({ title, description, href, children }: CardProps) {
-  const content = (
+export default function Card({ title, description, href }) {
+  const card = (
     <motion.div
-      whileHover={{ y: -4, boxShadow: '0 10px 20px rgba(0,0,0,0.1)' }}
-      className="border border-gray-700 rounded-lg p-6 bg-base text-text transition"
+      whileHover={{ y: -4, boxShadow: '0 10px 20px rgba(0,0,0,0.4)' }}
+      className="rounded-lg p-6 border border-lavender/20 bg-midnight text-lavender transition"
     >
       <h2 className="text-2xl font-semibold mb-2">{title}</h2>
-      <p className="text-base mb-4">{description}</p>
-      {children}
+      <p className="text-base">{description}</p>
     </motion.div>
   );
-  return href ? <Link href={href}>{content}</Link> : content;
+  return href ? <Link href={href}>{card}</Link> : card;
 }
