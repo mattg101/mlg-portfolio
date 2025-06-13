96f3e2ff4ef02fbc6e89c2f3950851d7a15d856f
mattg101 <mattgildner101@gmail.com>
2025-06-13 06:49:28 -0700

    Allow JPL image domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www-robotics.jpl.nasa.gov',
+        port: '',
+        pathname: '/media/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
